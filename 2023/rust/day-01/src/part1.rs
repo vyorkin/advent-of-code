dbd8a61349801496a47caa33580e1c8156e0f0db
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-06-04 15:03:54 +0300

    d1

--- 2023/rust/day-01/src/part1.rs
+++ 2023/rust/day-01/src/part1.rs
@@ -1,6 +1,24 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+fn recover_calibration_value(line: &str) -> u32 {
+    let mut digits =
+        line.chars().filter_map(|c| c.to_digit(10));
+
+    let x = digits.next().expect("first digit expected");
+    let y = digits.next_back().unwrap_or(x);
+
+    format!("{x}{y}")
+        .parse()
+        .expect("expected to have a number")
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String> {
+    let answer = input
+        .lines()
+        .map(recover_calibration_value)
+        .sum::<u32>();
+
+    Ok(answer.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +27,12 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("day 01 - test not implemented yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+";
+        assert_eq!("142", process(input)?);
         Ok(())
     }
 }
