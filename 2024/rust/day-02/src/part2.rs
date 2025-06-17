b7063c0cf7e88e58f56c8d8256ab118127437a3d
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-06-17 09:04:55 +0300

    Solve day-02 part2 of 2024 in Rust

--- 2024/rust/day-02/src/part2.rs
+++ 2024/rust/day-02/src/part2.rs
@@ -1,6 +1,87 @@
+use miette::Diagnostic;
+use thiserror::Error;
+
+#[derive(Error, Debug, Diagnostic)]
+pub enum AocError {
+    #[error(transparent)]
+    #[diagnostic(code(aoc::io_error))]
+    IoError(#[from] std::io::Error),
+
+    #[error("Parse error: `{0}`")]
+    #[diagnostic(code(aoc::parser_error))]
+    ParseError(String),
+}
+
+mod parser {
+    use nom::{
+        IResult, Parser, bytes::complete::tag,
+        character::complete::u32, multi::separated_list1,
+    };
+
+    use super::*;
+
+    fn report(input: &str) -> IResult<&str, Vec<u32>, ()> {
+        let mut parser = separated_list1(tag(" "), u32);
+        parser.parse(input)
+    }
+
+    pub fn parse(
+        input: &str,
+    ) -> Result<Vec<u32>, AocError> {
+        report(input).map(|x| x.1).map_err(|_| {
+            AocError::ParseError(input.to_string())
+        })
+    }
+}
+
+fn is_safe_monotonic<F>(xs: &[u32], cmp: F) -> bool
+where
+    F: Fn(u32, u32) -> bool,
+{
+    xs.windows(2).all(|w| {
+        let (prev, curr) = (w[0], w[1]);
+        let diff = curr.abs_diff(prev);
+        (1..=3).contains(&diff) && cmp(curr, prev)
+    })
+}
+
+fn remove_nth(xs: &[u32], n: usize) -> Vec<u32> {
+    let mut ys =
+        Vec::with_capacity(xs.len().saturating_sub(1));
+    ys.extend_from_slice(&xs[..n]);
+    ys.extend_from_slice(&xs[n + 1..]);
+    ys
+}
+
+fn is_safe(xs: &[u32]) -> bool {
+    xs.len() < 2
+        || is_safe_monotonic(xs, |curr, prev| curr > prev)
+        || is_safe_monotonic(xs, |curr, prev| curr < prev)
+}
+
+fn is_good_enough(xs: &[u32]) -> bool {
+    let ys: Vec<Vec<u32>> = xs
+        .iter()
+        .enumerate()
+        .map(|(i, _)| remove_nth(xs, i))
+        .collect();
+
+    ys.iter().any(|xs| is_safe(xs))
+}
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let reports = input
+        .lines()
+        .map(parser::parse)
+        .collect::<Result<Vec<Vec<u32>>, _>>()?;
+
+    let sum = reports
+        .iter()
+        .filter(|&r| is_good_enough(r))
+        .count();
+
+    Ok(sum.to_string())
 }
 
 #[cfg(test)]
@@ -8,11 +89,23 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_parser() -> miette::Result<()> {
+        let input = "7 6 4 2 1";
+        let expected = vec![7, 6, 4, 2, 1];
+        let actual = parser::parse(input)?;
+        assert_eq!(expected, actual);
+        Ok(())
+    }
+
+    #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("day 01 - test not implemented yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!("4", process(input)?);
         Ok(())
     }
 }
-
