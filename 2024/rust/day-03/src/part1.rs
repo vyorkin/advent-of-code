2b35cd04ebddfe521eeb1b8f9c7bdcfd9144a746
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-07-01 16:50:18 +0300

    Solve day-03 part2 of 2024 in Rust

--- 2024/rust/day-03/src/part1.rs
+++ 2024/rust/day-03/src/part1.rs
@@ -2,7 +2,7 @@ use miette::Diagnostic;
 use thiserror::Error;
 
 #[derive(Error, Debug, Diagnostic)]
-pub enum AocError {
+enum AocError {
     #[error(transparent)]
     #[diagnostic(code(aoc::io_error))]
     IoError(#[from] std::io::Error),
@@ -12,11 +12,19 @@ pub enum AocError {
     ParseError(String),
 }
 
+#[derive(Debug)]
+enum Opcode {
+    Mul(u32, u32),
+}
+
 mod parser {
     use nom::{
         IResult, Parser,
         branch::alt,
-        bytes::complete::{tag, take, take_until},
+        bytes::{
+            complete::{take, take_until},
+            tag,
+        },
         character::{char, complete::u32},
         combinator::map,
         multi::many0,
@@ -25,33 +33,34 @@ mod parser {
 
     use super::*;
 
-    fn mul(input: &str) -> IResult<&str, (u32, u32), ()> {
+    fn mul_opcode(
+        input: &str,
+    ) -> IResult<&str, Opcode, ()> {
         delimited(
             tag("mul("),
             separated_pair(u32, char(','), u32),
             char(')'),
         )
+        .map(|(x, y)| Opcode::Mul(x, y))
         .parse(input)
     }
 
-    fn find_mul(
-        input: &str,
-    ) -> IResult<&str, (u32, u32), ()> {
+    fn find_mul(input: &str) -> IResult<&str, Opcode, ()> {
         preceded(
             take_until("mul("),
-            map(mul, |pair| pair),
+            map(mul_opcode, |pair| pair),
         )
         .parse(input)
     }
 
-    fn mul_ops(
+    fn opcodes(
         input: &str,
-    ) -> IResult<&str, Vec<(u32, u32)>, ()> {
-        let skip_one = map(take(1usize), |_| None);
-        let next_mul = map(find_mul, Some);
+    ) -> IResult<&str, Vec<Opcode>, ()> {
+        let skip_one_char = map(take(1usize), |_| None);
+        let next_opcode = map(find_mul, Some);
 
         let (rest, matches) =
-            many0(alt((next_mul, skip_one)))
+            many0(alt((next_opcode, skip_one_char)))
                 .parse(input)?;
 
         let results =
@@ -62,8 +71,8 @@ mod parser {
 
     pub fn parse(
         input: &str,
-    ) -> Result<Vec<(u32, u32)>, AocError> {
-        mul_ops(input).map(|x| x.1).map_err(|_| {
+    ) -> Result<Vec<Opcode>, AocError> {
+        opcodes(input).map(|x| x.1).map_err(|_| {
             AocError::ParseError(input.to_string())
         })
     }
@@ -72,7 +81,10 @@ mod parser {
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
     let ops = parser::parse(input)?;
-    let sum = ops.iter().fold(0, |acc, (x, y)| acc + x * y);
+    let sum =
+        ops.iter().fold(0, |acc, opcode| match opcode {
+            Opcode::Mul(x, y) => acc + x * y,
+        });
     Ok(sum.to_string())
 }
 
--- 2024/rust/day-03/src/part2.rs
+++ 2024/rust/day-03/src/part2.rs
@@ -2,50 +2,135 @@ use miette::Diagnostic;
 use thiserror::Error;
 
 #[derive(Error, Debug, Diagnostic)]
-pub enum AocError {
+enum AocError {
     #[error(transparent)]
     #[diagnostic(code(aoc::io_error))]
     IoError(#[from] std::io::Error),
 
-    #[error("Parse error: `{0}`")]
+    #[error("Parse error:\n`{0}`.\nInput: `{1}`")]
     #[diagnostic(code(aoc::parser_error))]
-    ParseError(String),
+    ParseError(String, String),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+enum Opcode {
+    Do,
+    Dont,
+    Mul(u32, u32),
 }
 
 mod parser {
+    use nom::{
+        IResult, Parser,
+        branch::alt,
+        bytes::complete::tag,
+        character::{
+            char,
+            complete::{anychar, u32},
+        },
+        combinator::value,
+        multi::{many_till, many1},
+        sequence::{delimited, separated_pair},
+    };
+
     use super::*;
 
+    fn mul_opcode(
+        input: &str,
+    ) -> IResult<&str, Opcode, ()> {
+        delimited(
+            tag("mul("),
+            separated_pair(u32, char(','), u32),
+            char(')'),
+        )
+        .map(|(x, y)| Opcode::Mul(x, y))
+        .parse(input)
+    }
+
+    fn opcode(input: &str) -> IResult<&str, Opcode, ()> {
+        alt((
+            value(Opcode::Do, tag("do()")),
+            value(Opcode::Dont, tag("don't()")),
+            mul_opcode,
+        ))
+        .parse(input)
+    }
+
+    fn opcodes(
+        input: &str,
+    ) -> IResult<&str, Vec<Opcode>, ()> {
+        many1(many_till(anychar, opcode).map(|(_, op)| op))
+            .parse(input)
+    }
+
     pub fn parse(
-        _input: &str,
-    ) -> Result<Vec<(u32, u32)>, AocError> {
-        todo!()
+        input: &str,
+    ) -> Result<Vec<Opcode>, AocError> {
+        opcodes(input).map(|x| x.1).map_err(|e| {
+            AocError::ParseError(
+                e.to_string(),
+                input.to_string(),
+            )
+        })
     }
 }
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
     let ops = parser::parse(input)?;
-    let sum = ops.iter().fold(0, |acc, (x, y)| acc + x * y);
+    let (_, sum) = ops.iter().fold(
+        (true, 0),
+        |(enabled, sum), opcode| match (enabled, opcode) {
+            (false, Opcode::Do) => (true, sum),
+            (true, Opcode::Do) => (true, sum),
+            (false, Opcode::Dont) => (false, sum),
+            (true, Opcode::Dont) => (false, sum),
+
+            (true, Opcode::Mul(x, y)) => {
+                (true, sum + x * y)
+            }
+            (false, Opcode::Mul(_, _)) => (false, sum),
+        },
+    );
     Ok(sum.to_string())
 }
 
 #[cfg(test)]
 mod tests {
+    use miette::IntoDiagnostic;
     use rstest::rstest;
 
-    use super::*;
+    use super::{parser::parse, *};
 
     #[rstest]
-    #[case("mul(2,3)don't()-mul(3,3)+do()mul(2,2)", "10")]
+    #[case(
+        "mul(2,3)don't()-mul(3,3)+do()mul(2,2)",
+        vec![
+            Opcode::Mul(2, 3),
+            Opcode::Dont,
+            Opcode::Mul(3, 3),
+            Opcode::Do,
+            Opcode::Mul(2, 2),
+        ]
+    )]
     #[case(
         "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
-        "48"
+        vec![
+            Opcode::Mul(2, 4),
+            Opcode::Dont,
+            Opcode::Mul(5, 5),
+            Opcode::Mul(11, 8),
+            Opcode::Do,
+            Opcode::Mul(8, 5),
+        ]
     )]
     fn test_parser(
-        #[case] _input: &str,
-        #[case] _expected: &str,
+        #[case] input: &str,
+        #[case] expected: Vec<Opcode>,
     ) -> miette::Result<()> {
-        todo!()
+        let opcodes = parse(input).into_diagnostic()?;
+        assert_eq!(opcodes, expected);
+        Ok(())
     }
 
     #[test]
