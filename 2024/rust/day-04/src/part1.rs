8f7d54abacdbd4e0346d1127f2f6d9b9d6162ab7
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-07-02 16:28:09 +0300

    Add solution for day-04 part1 of 2024 in Rust

--- 2024/rust/day-04/src/part1.rs
+++ 2024/rust/day-04/src/part1.rs
@@ -1,6 +1,51 @@
+use std::collections::HashMap;
+
+const DIRECTIONS: [[(i32, i32); 3]; 8] = [
+    [(0, 1), (0, 2), (0, 3)],
+    [(0, -1), (0, -2), (0, -3)],
+    [(1, 1), (2, 2), (3, 3)],
+    [(1, -1), (2, -2), (3, -3)],
+    [(-1, 1), (-2, 2), (-3, 3)],
+    [(-1, -1), (-2, -2), (-3, -3)],
+    [(1, 0), (2, 0), (3, 0)],
+    [(-1, 0), (-2, 0), (-3, 0)],
+];
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 04 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let positions = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().map(move |(x, c)| {
+                ((x as i32, y as i32), c)
+            })
+        })
+        .collect::<HashMap<(i32, i32), char>>();
+
+    let mas = ['M', 'A', 'S'];
+
+    let result: usize = positions
+        .iter()
+        .filter(|(_, c)| **c == 'X')
+        .map(|((x, y), _)| {
+            DIRECTIONS
+                .iter()
+                .map(|offsets| {
+                    offsets
+                        .iter()
+                        .map(|(dx, dy)| {
+                            positions.get(&(x + dx, y + dy))
+                        })
+                        .enumerate()
+                        .all(|(ix, c)| mas.get(ix) == c)
+                })
+                .filter(|b| *b)
+                .count()
+        })
+        .sum();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -8,10 +53,30 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_process_simple() -> miette::Result<()> {
+        let input: &'static str = "..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....";
+
+        assert_eq!("4", process(input)?);
+        Ok(())
+    }
+
+    #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("day 04 - test not implemented yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input: &'static str = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+        assert_eq!("18", process(input)?);
         Ok(())
     }
 }
